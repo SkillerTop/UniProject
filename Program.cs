cab9c53ea5f044b30bf61cf1b078b6d436713af0
Skiller1337 <artemgaame@gmail.com>
2023-10-21 16:46:20 +0300

    lab4

--- Program.cs
+++ Program.cs
@@ -1,19 +1,52 @@
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder();
-builder.Services.AddTransient<CalcService>();
-builder.Services.AddTransient<TimeOfDayService>();
+
+builder.Services.AddTransient<Book>();
+builder.Services.AddTransient<User>();
+
+builder.Configuration.AddJsonFile("Route/Books.json");
 
 var app = builder.Build();
 
-app.MapGet("/", () =>
-{
-    var timeOfDayService = app.Services.GetRequiredService<TimeOfDayService>();
-    var calcService = app.Services.GetRequiredService<CalcService>();
+app.Map("/", () => "Index Page");
 
-    return Results.Text($"Time: {timeOfDayService.GetTimeOfDay()}\n" +
-                        $"Sum: {calcService.Add(12, 2)}\n" +
-                        $"Subtraction: {calcService.Subtract(15, 3)}\n" +
-                        $"Dividing: {calcService.Division(21, 3)}\n" +
-                        $"Multiplication: {calcService.Multiplication(9, 2)}", "text/plain");
+app.Map("/Library", async (context) =>
+{
+    await context.Response.WriteAsync("Welcome to my library");
 });
+
+app.Map("/Library/books", async (context) =>
+{
+    var booksList = app.Configuration.GetSection("Books").Get<List<Book>>();
+
+    foreach (var book in booksList)
+    {
+        await context.Response.WriteAsync($"Book: {book.Title} by {book.Author}\n");
+    }
+});
+
+app.Map("/Library/Profile/{id:int?}", (int? id) =>
+{
+    var configFileName = id.HasValue && id >= 0 && id <= 5
+        ? $"reader{id}.json"
+        : "reader0.json";
+
+    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Route/Users", configFileName);
+
+    if (File.Exists(filePath))
+    {
+        var userInfo = JsonConvert.DeserializeObject<User>(File.ReadAllText(filePath));
+        return $"Name: {userInfo.Name}, Age: {userInfo.Age}";
+    }
+    else
+    {
+        return "File configuration of user is not found.";
+    }
+});
+
+app.MapGet("/allmaps", (IEnumerable<EndpointDataSource> endpointSources) =>
+    string.Join("\n", endpointSources.SelectMany(source => source.Endpoints)));
 
 app.Run();
